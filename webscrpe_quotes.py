5b4d897665fcd443247d442d8044f56f62525c0f
M. Ahmed <20860945+sasoftwarerl@users.noreply.github.com>
2018-02-13 16:33:47 -0500

    Update webscrpe_quotes.py

--- webscrpe_quotes.py
+++ webscrpe_quotes.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 # link we are using: http://quotes.toscrape.com/page/1/
 
 
-def get_quotes(max_pages):
+def get_quotes(all_pages):
     page = 1
-    while page <= max_pages:
+    while page <= all_pages:
         url = 'http://quotes.toscrape.com/page/' + str(page) + '/'
         get_source = requests.get(url)
         plain_text = get_source.text
