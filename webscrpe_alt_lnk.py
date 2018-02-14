d795892c89186aa940c1276a48e8d091d61084ee
M. Ahmed <20860945+sasoftwarerl@users.noreply.github.com>
2018-02-13 20:15:40 -0500

    Update webscrpe_alt_lnk.py

--- webscrpe_alt_lnk.py
+++ webscrpe_alt_lnk.py
@@ -9,26 +9,22 @@ def get_books(all_pages):
     while page <= all_pages:
         url = 'http://books.toscrape.com/catalogue/page-' + str(page) + '.html'
         get_source = requests.get(url)
-        plain_text = get_source.text
-        soap_scrape = BeautifulSoup(plain_text, 'html.parser')
+        soap_scrape = BeautifulSoup(get_source.text, 'html.parser')
         for link in soap_scrape.find_all('article', {'class': 'product_pod'}):
             child_link = link.findChild("h3").findChild("a")
-            href = 'http://books.toscrape.com/catalogue/' + child_link.get('href')
-            print(href)
-            get_single_page(href)
+            print('http://books.toscrape.com/catalogue/' + child_link.get('href'))
+            get_book_info('http://books.toscrape.com/catalogue/' + child_link.get('href'))
         page += 1
 
 
-def get_single_page(book_url):
+def get_book_info(book_url):
     get_source = requests.get(book_url)
-    plain_text = get_source.text
-    soup = BeautifulSoup(plain_text, 'html.parser')
+    soup = BeautifulSoup(get_source.text, 'html.parser')
     for book_name in soup.find_all('h1'):
         print("\n")
         print(book_name.string)
     for links in soup.find_all('a'):
-        href = 'http://books.toscrape.com/catalogue/' + links.get('href')
-        print(href)
+        print('http://books.toscrape.com/catalogue/' + links.get('href'))
 
 get_books(2)
 
